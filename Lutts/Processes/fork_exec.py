29f9090d678bcb8ae8ace5d3e8515e7790f1cb69
StognienkoVladislav <stognienkovv@gmail.com>
2017-06-17 15:26:20 +0400

    commit

---
+++ Lutts/Processes/fork_exec.py
@@ -0,0 +1,15 @@
+#Запускает программы, пока не будет нажата клавиша 'q'
+
+import os
+
+parm = 0
+while True:
+    parm += 1
+    pid = os.fork()
+    if pid == 0:                                                #копия процесса
+        os.execlp('python', 'python', 'child.py', str(parm))    #подменить прогр
+        assert False, 'error starting program'                  #возврата быть не дожноы
+
+    else:
+        print('Child is', pid)
+        if input() == 'q': break

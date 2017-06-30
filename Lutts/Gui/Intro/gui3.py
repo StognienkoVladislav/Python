4717ac437759153950c9cb80820d66f476d42cd0
StognienkoVladislav <stognienkovv@gmail.com>
2017-06-30 18:30:52 +0400

    gui trash

---
+++ Lutts/Gui/Intro/gui3.py
@@ -0,0 +1,10 @@
+import sys
+from tkinter import *
+
+def quit():                                                    #собственный обработчик событий
+    print('Hello, I must be going...')
+    sys.exit()                                                 #закрыть окно и завершить процесс
+
+widget = Button(None, text='Hello event world', command = quit)
+widget.pack()
+widget.mainloop()

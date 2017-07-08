60416985792841f732afdc713a34cb822f2da566
StognienkoVladislav <stognienkovv@gmail.com>
2017-07-08 15:44:48 +0400

    canvas draw

---
+++ Lutts/Gui/Tour/Canvas/canvasDraw_tags.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from Lutts.Gui.Tour.Canvas import canvasDraw
+import time
+
+class CanvasEentsDemo(canvasDraw.CanvasEventsDemo):
+    def __init__(self, parent=None):
+        canvasDraw.CanvasEventsDemo.__init__(self, parent)
+        self.canvas.create_text(100, 10, text='Press o anr r to move shapes')
+        self.canvas.master.bind('<KeyPress-o>', self.onMoveOvals)
+        self.canvas.master.bind('<KeyPress-r>', self.onMoveRectangles)
+        self.kinds = self.create_oval_tagged, self.create_rectangle_tagged
+
+    def create_oval_tagged(self, x1, y1, x2, y2):
+        objectId = self.canvas.create_oval(x1, y1, x2, y2)
+        self.canvas.itemconfig(objectId, tag='ovals', fill='blue')
+        return objectId
+
+    def create_rectangle_tagged(self, x1, y1, x2, y2):
+        objectId = self.canvas.create_rectangle(x1, y1, x2, y2)
+        self.canvas.itemconfig(objectId, tag='rectangles', fill='red')
+        return objectId
+
+    def onMoveOvals(self, event):
+        print('moving ovals')
+        self.moveInSquares(tag='ovals')             #переместить все овалы с данным тегом
+
+    def onMoveRectangles(self, event):
+        print('moving rectangles')
+        self.moveInSquares(tag='rectangles')
+
+    def moveInSquares(self, tag):
+        for i in range(5):
+            for (diffx, diffy) in [(+20, 0), (0, +20), (-20, 0), (0, -20)]:
+                self.canvas.move(tag, diffx, diffy)
+                self.canvas.update()                #принудительно обновить изображение
+                time.sleep(0.25)                    #не блоч пауза
+
+if __name__ == '__main__':
+    CanvasEentsDemo()
+    mainloop()

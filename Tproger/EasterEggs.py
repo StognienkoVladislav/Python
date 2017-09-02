467f51f4bebbc0f94d639320251a0165a9fbfa96
stognienkovladislav <stognienkovv@gmail.com>
2017-09-02 20:29:19 +0300

    EaserEggs

---
+++ Tproger/EasterEggs.py
@@ -0,0 +1,52 @@
+
+
+#Дзен Пайтона
+import this
+
+print(this)
+
+
+
+#Пример
+
+love = this
+print(this is love)
+
+print(love is True)
+
+print(love is False)
+
+print(love is not True or False)
+
+
+import antigravity
+
+def main():
+
+    antigravity.fly()
+
+if __name__ == '__main__':
+    main()
+
+
+from math import pi
+π = pi
+r = 5
+area = π * r ** 2
+
+print(area)
+
+résumé = 'knows Python'
+print('Python' in résumé)
+
+
+infinity = float('infinity')
+print(hash(infinity))
+
+print(hash(float('-inf')))
+
+
+from antigravity import geohash
+#Your location, a date and that date`s (or most recent) DJIA opening
+
+print(geohash(37.421542, -122.085589, b'2005-05-26-10458.68'))

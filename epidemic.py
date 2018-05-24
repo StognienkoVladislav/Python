0327f759f498b7bf35de76300eef9208112392bb
Vladislav Stognienko <stognienkovv@gmail.com>
2018-05-24 11:32:55 +0300

    epidemic add commits

--- epidemic.py
+++ epidemic.py
@@ -2,32 +2,41 @@ import random
 import sys
 
 
-def algor(n, i):
-    checker = 0
+def epidemic(n, i):
+    # done_checker - счетчик для успешных передач данных всем нодам
+    done_checker = 0
     for _ in range(i):
+        """
+        mass - изначальный нулевой массив нод
+        workers - ноды, в которых есть хотя бы 1 пакет (изначально данные передаються с ноды с индексом 0)
+        X - кол-во рандомных узлов
+        """
         mass = [0]*n
-        # print(mass)
-
         workers = [0]
+        X = 4
 
-        X = 3
-
+        # пока есть не залоченные ноды
         while workers:
-
+            # check - нода, от которой рандомно передаютсья X пакетов другим нодам
             check = workers.pop(0)
+            # При mass[check] == 2, нода закрываеться
             if mass[check] <= 1:
                 mass[check] += 1
             else:
                 continue
             for _ in range(X):
+                # Рандомно выбираем Х-кол-во нод, которым передадим данные, исключая текущую ноду
                 r = random.choice([x for x in range(len(mass)) if x != check])
+                # Добавляем их для следующих итераций
                 workers.append(r)
+        # Если сумма значенний в массиве равна 2*N(кол-во нод), то все ноды закрыты
+        # инкрементируем счетчик
         if sum(mass) == 2*n:
-            checker += 1
-
-    print((checker/i)*100)
+            done_checker += 1
+    # Процент успешных передач
+    print('In {}% cases all nodes received the packet'.format((done_checker/i)*100))
 
 
 if __name__ == '__main__':
-    algor(20, 1000)
-    # algor(int(sys.argv[1]), int(sys.argv[2]))
+    epidemic(20, 1000)
+    # epidemic(int(sys.argv[1]), int(sys.argv[2]))
--- epidemic_up.py
+++ epidemic_up.py
@@ -3,30 +3,48 @@ import sys
 
 
 def algor(n, i):
-    checker = 0
+    # done_checker - счетчик для успешных передач данных всем нодам
+    done_checker = 0
 
     for _ in range(i):
+        """
+        ultra_check - массив с нодами, которые получили 1 пакет
+        mass - изначальный нулевой массив нод
+        X - кол-во рандомных узлов
+        """
         ultra_check = []
         mass = [0]*n
 
         X = 2
-
+        # Пока все ноды не получат пакет данных
         while len(ultra_check) != len(mass):
 
             for _ in range(X):
+                # Рандомно выбираем Х-кол-во нод, которым передадим данные, исключая текущую ноду
                 try:
                     r = random.choice([x for x in range(len(mass)) if x not in ultra_check])
                     mass[r] = 1
                     ultra_check.append(r)
+                # Если все ноды уже заполненны и не кому передавать данные - выходим из цикла
                 except:
                     break
 
+        # Если сумма значенний в массиве равна N(кол-во нод), то все ноды закрыты
+        # инкрементируем счетчик
         if sum(mass) == n:
-            checker += 1
+            done_checker += 1
 
-    print((checker/i)*100)
+    print('In {}% cases all nodes received the packet'.format((done_checker / i) * 100))
+
+    """
+    Возможно я не так понял задание:
+    <<Вторым обязательным условием является написать алгоритм пересылки 
+    пакета который хотя бы на 1% лучше, и продемонстрировать это:>>
+    В мое случае, нода выбирает исключительно ту ноду, которой еще не передавался пакет и нет
+    возможности закрытия другой ноды(т.к. одной и той же не передаеться пакет данных)
+    """
 
 
 if __name__ == '__main__':
-    # algor(20, 1000)
-    algor(int(sys.argv[1]), int(sys.argv[2]))
+    algor(20, 1000)
+    #algor(int(sys.argv[1]), int(sys.argv[2]))
